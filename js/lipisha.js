ebfbae5570b7f6ebe83557f2c2d9fadeecde80a0
moshthepitt <kelvin@jayanoris.com>
2016-09-17 11:02:54 +0300

    add one that works after ajax calls

--- js/lipisha.js
+++ js/lipisha.js
@@ -21,5 +21,30 @@ jQuery(function(jQuery) {
     jQuery('.lipisha-validation').removeClass('text-danger text-success');
     jQuery('.lipisha-validation').addClass(jQuery('.has-error').length ? 'text-danger' : 'text-success');
   });
+});
 
+
+jQuery(document).ajaxComplete(function() {
+  jQuery('[data-numeric]').payment('restrictNumeric');
+  jQuery('.cc-number').payment('formatCardNumber');
+  jQuery('.cc-exp').payment('formatCardExpiry');
+  jQuery('.cc-cvc').payment('formatCardCVC');
+
+  jQuery.fn.toggleInputError = function(erred) {
+    this.parent('.form-group').toggleClass('has-error', erred);
+    return this;
+  };
+
+  jQuery('form.checkout').submit(function(e) {
+    e.preventDefault();
+
+    var cardType = jQuery.payment.cardType(jQuery('.cc-number').val());
+    jQuery('.cc-number').toggleInputError(!jQuery.payment.validateCardNumber(jQuery('.cc-number').val()));
+    jQuery('.cc-exp').toggleInputError(!jQuery.payment.validateCardExpiry(jQuery('.cc-exp').payment('cardExpiryVal')));
+    jQuery('.cc-cvc').toggleInputError(!jQuery.payment.validateCardCVC(jQuery('.cc-cvc').val(), cardType));
+    jQuery('.cc-brand').text(cardType);
+
+    jQuery('.lipisha-validation').removeClass('text-danger text-success');
+    jQuery('.lipisha-validation').addClass(jQuery('.has-error').length ? 'text-danger' : 'text-success');
+  });
 });
